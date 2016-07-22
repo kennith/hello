79bb59c37b27a3468754dd7e67fe7acc76c24888
Kennith Leung <kennith.leung@gmail.com>
2016-07-22 10:23:06 -0700

    Add task to create dist

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,43 @@ var less   = require('gulp-less');
 var bs     = require('browser-sync');
 var concat = require('gulp-concat');
 var sourcemaps = require('gulp-sourcemaps');
-var minify = require('gulp-minify');
+var uglify  = require('gulp-uglify');
+var del = require('del');
+var cleanCSS = require('gulp-clean-css');
 
 var dist = 'dist';
 
-gulp.task('dist', function() {
-    return gulp.src('app/*')
-        .pipe(gulp.dest('dist'));
+gulp.task('dist', ['clean:dist'], function() {
+    var scripts = ['bower_components/jquery/dist/jquery.min.js', 'assets/js/**/*.js']
+
+    gulp.src('app/*')
+        .pipe(gulp.dest(dist));
+
+    gulp.src('assets/less/app.less')
+            // .pipe(minify())
+            .pipe(sourcemaps.init())
+            .pipe(less({compress: true}).on('error', errorHandler))
+            .pipe(sourcemaps.write())
+            .pipe(gulp.dest(dist + '/css/'))
+
+    gulp.src('assets/css/*.css')
+            .pipe(cleanCSS())
+            .pipe(concat('all.css'))
+            // .pipe(uglify())
+            .pipe(gulp.dest(dist + '/css/'));
+
+    gulp.src(scripts)
+        .pipe(concat('app.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(dist + '/js/'));
+
+    return true;
+        // .pipe(['less:dist', 'scripts:dist', 'css:dist']);
 });
+
+gulp.task('clean:dist', function() {
+    return del(dist);
+})
 
 gulp.task('default', ['scripts', 'less', 'css'], function() {
 
@@ -28,25 +57,10 @@ gulp.task('less', function() {
         .pipe(bs.stream());
 });
 
-gulp.task('less:dist', function() {
-    return gulp.src('assets/less/app.less')
-        // .pipe(minify())
-        .pipe(sourcemaps.init())
-        .pipe(less({compress: true}).on('error', errorHandler))
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('dist/css/'))
-});
-
 gulp.task('css', function() {
     return gulp.src('assets/css/*.css')
         .pipe(concat('all.css'))
         .pipe(gulp.dest('app/css/'));
-});
-
-gulp.task('css:dist', function() {
-    return gulp.src('assets/css/*.css')
-        .pipe(concat('all.css'))
-        .pipe(gulp.dest('dist/css/'));
 });
 
 gulp.task('scripts', function() {
@@ -54,13 +68,6 @@ gulp.task('scripts', function() {
     return gulp.src(scripts)
         .pipe(concat('app.js'))
         .pipe(gulp.dest('app/js/'));
-});
-
-gulp.task('scripts:dist', function() {
-    var scripts = ['bower_components/jquery/dist/jquery.min.js', 'assets/js/**/*.js']
-    return gulp.src(scripts)
-        .pipe(concat('app.js'))
-        .pipe(gulp.dest('dist/js/'));
 });
 
 gulp.task('serve', ['default'], function() {
