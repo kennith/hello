fa42bc4d0f276b358a2257f404f6d98d8c345022
Kennith Leung <kennith.leung@gmail.com>
2016-08-08 10:31:02 -0700

    Add new inspiring quotes

--- app/js/app.js
+++ app/js/app.js
@@ -5,17 +5,29 @@ void 0!==c?e&&"set"in e&&void 0!==(d=e.set(a,c,b))?d:a[b]=c:e&&"get"in e&&null!=
 
 var quotes = [
 {
-    'author': 'Dweck',
-    'quote': 'Growth mindset 1.'
+    'author': 'Steve Jobs',
+    'quote': 'Your time is limited. Don’t waste it living someone else’s life.'
 },
 {
-    'author': 'Dweck',
-    'quote': 'Growth mindset 2.'
-}
+    'author': 'Bill Gates',
+    'quote': 'Your most unhappy customers are your greatest source of learning.'
+},
+{
+    'author': 'Carol S. Dweck',
+    'quote': 'Did I win? Did I lose? Those are the wrong questions. The correct question is: Did I make my best effort?'
+},
+{
+    'author': 'David Allen',
+    'quote': '“Much of the stress that people feel doesn\'t come from having too much to do. It comes from not finishing what they\'ve started.'
+},
+{
+    'author': 'Warren Buffet',
+    'quote': 'It takes 20 years to build a reputation and five minutes to ruin it. If you think about that, you\'ll do things differently.'
+},
 ];
 
 $(function() {
-   // setQuote('.quote', '.author');
+   setQuote('.quote', '.author');
    $('#sw-reload').click(function() {
         console.log('reaload');
         document.location.reload(true);
