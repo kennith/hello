8d5b54774c2d6ac3318b53ff9ce405c5f44fe18b
Kennith Leung <kennith.leung@gmail.com>
2023-09-23 16:11:51 -0700

    build: setup gh page build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
+  app: {
+    baseURL: '/hello'
+  },
   devtools: { enabled: true },
   modules: [
     '@nuxtjs/tailwindcss',
     'nuxt-icon'
-  ]
+  ],
+  nitro: {
+    output: {
+      publicDir: 'docs'
+    }
+  }
 })
